87bece6a651d5214387b16ff7d923770238156cf
KLRBDN <kilirabodon1@mail.ru>
2021-11-16 10:07:29 +0500

    Added BotCommand CompleteTask, getDayTasks(), getTodayTasks(), placed getDay() and getToday() in Day.java and remade them from scratch

--- src/main/java/org/example/GetTasks.java
+++ src/main/java/org/example/GetTasks.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class GetTasks implements BotCommand {
 
     @Override
@@ -15,8 +18,20 @@ public class GetTasks implements BotCommand {
     @Override
     public AnswerHandler exec() {
         var strBuilder = new StringBuilder();
-        for (Task task : YearsDataBase.getToday().getTasks())
+        for (Task task : getTodayTasks())
             strBuilder.append(task.name + ": " + task.timeInterval.toString() + "\n");
         return new StandartAnswerHandler(strBuilder.toString());    
+    }
+
+    public static ArrayList<Task> getTodayTasks() {
+        return Day.getToday().getTasks();
+    }
+
+    public static ArrayList<Task> getDayTasks(LocalDate date) {
+        return Day.getDay(date).getTasks();
+    }
+
+    public static ArrayList<Task> getDayTasks(int day, int month, int year) {
+        return Day.getDay(day, month, year).getTasks();
     }
 }
